fc640524eeb7175c8fe0fdfeee6e7f7a8991337d
Kajetan Bojko <kbojko@ebay.com>
2018-11-11 21:55:46 -0800

    removed debug output Serial

--- Remapper.ino
+++ Remapper.ino
@@ -48,32 +48,11 @@ OutputModifier* rightGui = &outputRightAlt;
 
 class KbdRptParser : public KeyboardReportParser
 {
-    void PrintKey(uint8_t mod, uint8_t key);
-
   protected:
     void OnControlKeysChanged(uint8_t before, uint8_t after);
 
     void OnKeyDown	(uint8_t mod, uint8_t key);
     void OnKeyUp	(uint8_t mod, uint8_t key);
-};
-
-void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
-{
-  MODIFIERKEYS mod;
-  *((uint8_t*)&mod) = m;
-  Serial.print((mod.bmLeftCtrl   == 1) ? "C" : " ");
-  Serial.print((mod.bmLeftShift  == 1) ? "S" : " ");
-  Serial.print((mod.bmLeftAlt    == 1) ? "A" : " ");
-  Serial.print((mod.bmLeftGUI    == 1) ? "G" : " ");
-
-  Serial.print(" >");
-  PrintHex<uint8_t>(key, 0x80);
-  Serial.print("< ");
-
-  Serial.print((mod.bmRightCtrl   == 1) ? "C" : " ");
-  Serial.print((mod.bmRightShift  == 1) ? "S" : " ");
-  Serial.print((mod.bmRightAlt    == 1) ? "A" : " ");
-  Serial.println((mod.bmRightGUI    == 1) ? "G" : " ");
 };
 
 bool isShiftPressed() {
@@ -136,7 +115,6 @@ void noShift(uint8_t code) {
 
 void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
-  Serial.print("DN ");
   OutputKeystroke* output = convertOemToAscii(key);
 
   switch (output->modifierAction) {
@@ -149,20 +127,15 @@ void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
     case ALT: withAlt(output->code);
     break;
   }
-
-  PrintKey(mod, key);
 }
 
 void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
 {
-  Serial.print("UP ");
   OutputKeystroke* output = convertOemToAscii(key);
 
   if (output->modifierAction == NONE) {
     Keyboard.release(output->code);
   }
-
-  PrintKey(mod, key);
 }
 
 void handleModifierChange(uint8_t modifierState, uint8_t modifierCode) {
@@ -182,43 +155,35 @@ void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
   *((uint8_t*)&afterMod) = after;
 
   if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
-    Serial.println("LeftCtrl changed");
     handleModifierChange(afterMod.bmLeftCtrl, leftCtrl->code);
     leftCtrl->isPressed = afterMod.bmLeftCtrl;
   }
   if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
-    Serial.println("LeftShift changed");
     handleModifierChange(afterMod.bmLeftShift, leftShift->code);
     leftShift->isPressed = afterMod.bmLeftShift;
   }
   if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
-    Serial.println("LeftAlt changed");
     handleModifierChange(afterMod.bmLeftAlt, leftAlt->code);
     leftAlt->isPressed = afterMod.bmLeftAlt;
   }
   if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
-    Serial.println("LeftGUI changed");
     handleModifierChange(afterMod.bmLeftGUI, leftGui->code);
     leftGui->isPressed = afterMod.bmLeftGUI;
   }
 
   if (beforeMod.bmRightCtrl != afterMod.bmRightCtrl) {
-    Serial.println("RightCtrl changed");
     handleModifierChange(afterMod.bmRightCtrl, rightCtrl->code);
     rightCtrl->isPressed = afterMod.bmRightCtrl;
   }
   if (beforeMod.bmRightShift != afterMod.bmRightShift) {
-    Serial.println("RightShift changed");
     handleModifierChange(afterMod.bmRightShift, rightShift->code);
     rightShift->isPressed = afterMod.bmRightShift;
   }
   if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
-    Serial.println("RightAlt changed");
     handleModifierChange(afterMod.bmRightAlt, rightAlt->code);
     rightAlt->isPressed = afterMod.bmRightAlt;
   }
   if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
-    Serial.println("RightGUI changed");
     handleModifierChange(afterMod.bmRightGUI, rightGui->code);
     rightGui->isPressed = afterMod.bmRightGUI;
   }
@@ -226,24 +191,14 @@ void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
 }
 
 USB     Usb;
-//USBHub     Hub(&Usb);
 HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
 
 KbdRptParser Prs;
 
 void setup()
 {
-  Serial.begin( 9600 );
-#if !defined(__MIPSEL__)
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-#endif
-  Serial.println("Start");
-
-  if (Usb.Init() == -1)
-    Serial.println("OSC did not start.");
-
+  Usb.Init();
   delay( 200 );
-
   HidKeyboard.SetReportParser(0, &Prs);
   Keyboard.begin();
 }
